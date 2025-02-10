c07a61bcb7ec849c4536526341659cd079a2c78c
farhawajid05 <32264097+farhawajid05@users.noreply.github.com>
2025-02-10 10:31:07 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -3,6 +3,6 @@ public class Welcome{
 public static void main(String[] args){
 System.out.println("Welcome All");
 System.out.println("Changes are made to the code");
-System.out.println("Data Added");
+System.out.println("Data Added in developer branch");
 }
 }
