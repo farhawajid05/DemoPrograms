1bd26c34cc20707741b07f662fc208c4bbba5305
farhawajid05 <32264097+farhawajid05@users.noreply.github.com>
2025-02-10 10:53:26 +0530

    Delete Welcome.java

--- Welcome.java
+++
@@ -1,8 +0,0 @@
-
-public class Welcome{
-public static void main(String[] args){
-System.out.println("Welcome All");
-System.out.println("Changes are made to the code");
-System.out.println("Data Added are included");
-}
-}
