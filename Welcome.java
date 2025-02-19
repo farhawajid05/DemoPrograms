986dc2370af6576406e588c293967e5d44dacda5
farhawajid05 <farha.wajid05@gmail.com>
2025-02-19 06:50:37 +0000

    Commit

--- Welcome.java
+++ Welcome.java
@@ -2,5 +2,6 @@ public class Welcome{
 public static void main(String[] args){
 System.out.println("Welcome All");
 System.out.println("Changes are added");
+System.out.println("Content added");
 }
 }
